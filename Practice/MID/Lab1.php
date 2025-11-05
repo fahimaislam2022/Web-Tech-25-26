9bacf05aefe103860f27aba91eb0adedb7d64852
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-11-05 11:27:53 +0600

    new file uploaded

--- Practice/MID/Lab1.php
+++ Practice/MID/Lab1.php
@@ -1,163 +1,59 @@
 <!DOCTYPE html>
- 
+<html>
 <head>
-        <title>
-            WT_0
-</title>
+    <title>
+        WT_O
+    </title>
 </head>
-<style>
-    body {
-        background-color:LavenderBlush;
- 
-    }
-    div{
-        width:800px;
-        height:950px;
-        padding:10px;
-        border:4px solid;
-        margin:0;
-        background-color:white;
-    }
-   
-</style>
- 
 <body>
- <center>
-    <div>
-<table>
- <h1 style ="color:red;font-size:30px">Student Registration Information</h1>
- <hr color = "Red";border =8px soild>
+    <center>
+        <h1>Blood Donation Management System</h1>
+    </center>
+    <center>
+        <h2>Donate Blood And Save A Life </h2>
+    </center>
+    <h1>Blood Donation Form</h1>
+    <p>Welcome to Blood Donation Form<br><br>Kindly fillup the Information Correctly
+    <br><br>
+    <table>
+     <tr>
+     <td>Enter Your Name:</td>
+     <td><input type="text"></td>
+     </tr>
+    Enter Your Age: 
+    <input type="number">
+    <br><br>
+    Enter Your Weight: 
+    <input type="number">
+    <br><br>
+    Gender:
+    <input type="checkbox">Male
+    <input type="checkbox">Female
+    <br><br>
+    Enter Your DOB:
+    <input type="date">
+    <br><br>
+    Are you interested to donate Blood?:
+    <input type="checkbox">YES 
+    <input type="checkbox">NO 
+    <br><br>
+    Blood Group:
+    <input type="radio">A+
+    <input type="radio">A-
+    <input type="radio">B+
+    <input type="radio">B-
+    <input type="radio">O+
+    <input type="radio">0-
+    <input type="radio">AB+
+    <input type="radio">AB-
 
-<tr>
-<td>Enter Your Frist Name:</td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
- 
-<tr>
-<td>Enter Your Last Name:</td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
- 
- 
-<tr>
-<td>Enter Your Student ID:</td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
- 
- 
-<tr>
-<td>Program/Major:</td>
-</tr>
+    <br><br>
+    Enter Your Last Blood Donation Date:
+    <input type="date">
+    
 
-<tr>
-        <td><select name="sub" id="sub">
-            <option value="CSE">BSc in CSE</option>
-                        <option value="EEE">BSc in EEE</option>
-                                    <option value="BBa">BSc in BBA</option>
-                                                <option value="English">BSc in ENGLISH</option>
-        </select></td>
-    </tr>
-
-
-<tr>
-<td>Department:</td>
-</tr>
-<tr>
-        <td><select name="sub" id="sub">
-            <option value="FST">FST</option>
-                        <option value="FBA">FBA</option>
-                                    <option value="FA">FA</option>
-                                                <option value="FP">FP</option>
-        </select></td>
-    </tr>
-
-
-<tr>
-<td>Phone:</td>
-</tr>
-<tr>
-<td><input type="number"></td>
-</tr>
-<tr>
-
-<tr>
-<td>University Email:</td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
-<tr>
-
-<tr>
-<td>Create Password (8 Characters)</td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
-<tr>
-
-<tr>
-<td>Confrim Password </td>
-</tr>
-<tr>
-<td><input type="text"></td>
-</tr>
-<tr>
-
-<tr>
-<td>Select Semester</td>
-</tr>
-
-<td><input type="radio" name="PC">Summer 2024
-    <input type="radio" name="PC">Fall 2024
-    <input type="radio" name="PC">Spring 2025
-    <input type="radio" name="PC">Other/Special Term
-</td>
-
-</table>
-
-<tr>
-<td>Requested Credit Load(Max 15)</td>
-</tr>
-<tr>
+    <center><input type="submit"></center>
     <br>
-<td><input type="number" placeholder="e.g, 9 or 12"></td>
-</tr>
-
-<br>
-<tr> 
-<input type="checkbox">I required academic advising before final registration 
-    </tr> 
-    <h1 style ="color:red;font-size:30px">Course Selection</h1>
- <hr color = "Red";border =4px soild>
- <td>
-    <input type="checkbox">Math1201 (Calculus l)
-    <input type="checkbox">CS 2105(Data Strutures)
-    <input type="checkbox">ECON 1001 (Microeconomics)
-    <br>
-    <input type="checkbox">PHY 1102(Physic Lab)
-</td>
-<br><br>
-<tr>
-<td>Comments/ Sepcial Requests
-</tr>
-<br>
-<tr>
-<td><input type="text">
-<br><br>
- <center><input type="submit"></center>
-<br>
-   <center> <input type="reset"></center>
-    </td>
-</tr>
-</center>
-</div>
-</body>
+    <center><input type="reset"></center>
+    </body>
 </html>
- 
