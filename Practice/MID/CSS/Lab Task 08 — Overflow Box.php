60b62f43835e2576da36248ec4f348adc83bdf92
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-11-22 21:44:36 +0600

    task 8

---
+++ Practice/MID/CSS/Lab Task 08 — Overflow Box.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Overflow Box</title>
+
+    <style>
+        .box{
+            width: 300px;
+            height: 200px;
+            border: 2px solid black;
+            padding: 10px;
+            overflow: auto; /* overflow behavior */
+        }
+    </style>
+
+</head>
+
+<body>
+
+<div class="box">
+    <p>Very long text inside the box... Very long text inside the box... Very long text inside the box...</p>
+</div>
+
+</body>
+</html>
