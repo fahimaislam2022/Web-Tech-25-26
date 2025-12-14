5e50d1948b7f494a82ed4e31e3a04e1b503a652e
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-12-14 22:14:51 +0600

    basic php

---
+++ Practice/FINAL/php basic.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHP Code</title></head>
+ 
+<body>
+<h1>This is our 1st php code </h1>    
+ 
+</body>
+ 
+ 
+<?php
+ 
+echo "Helllo World";
+echo "<br>";
+ 
+$name="Hamim";
+$age="25";
+echo "<br>";
+ 
+echo "I am" .$name. "  and My age is : " .$age;
+ 
+echo "<br>";
+echo "<br>";
+ 
+$a=30;
+$b=60;
+ 
+if($a>$b)
+{
+    echo "a is bigger then b <br>";
+}
+ 
+else
+{
+    echo "b is bigger then a <br>";
+}
+ 
+echo "Hello World!!!! <br>";
+echo "This is the new line. <br> "
+ 
+ 
+?>
+ 
+ 
+</html>
