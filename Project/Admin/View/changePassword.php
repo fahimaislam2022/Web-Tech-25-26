b41472155515f5cabcb24c3d7b2ab491a7404186
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-12-29 12:30:36 +0600

    change

--- Project/Admin/View/changePassword.php
+++ Project/Admin/View/changePassword.php
@@ -19,29 +19,7 @@
 
 <p><a href="dashboard.html">Back</a></p>
 
-<script>
-document.getElementById("passwordForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    var c = document.getElementById("currentpassword").value;
-    var n = document.getElementById("newpassword").value;
-    var x = document.getElementById("confirmPassword").value;
-    if(c === "" || n === "" || x === "") {
-        alert("All fields required.");
-        return;
-    }
-    if(n.length < 6) {
-        alert("Password must be 6+ characters.");
-        return;
-    }
-    if(n !== x) {
-        alert("Passwords do not match.");
-        return;
-    }
 
-    alert("Password changed (prototype).");
-    window.location.href = "login.html";
-});
-</script>
 </center>
 </body>
 </html>
